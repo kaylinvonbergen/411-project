d5cbbd49db068bdb63d22c1c896b99c5e9c1138b
Anna LaPrade <annalaprade@crc-dot1x-nat-10-239-159-112.bu.edu>
2024-12-02 13:58:28 -0500

    minor edits to db table setup

--- sql/create_team_table.sh
+++ sql/create_team_table.sh
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS teams;
 CREATE TABLE teams (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     team TEXT NOT NULL UNIQUE,
-    correct INTEGER DEFAULT 0,
+    current_score INTEGER DEFAULT 0,
+    favorite_game_category INTEGER,
     deleted BOOLEAN DEFAULT FALSE
     FOREIGN KEY (favorite_game_category) REFERENCES categories(id)
 );

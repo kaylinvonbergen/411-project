65f07d4195e8e92b99cf9283575e98db55f42278
Anna LaPrade <annalaprade@crc-dot1x-nat-10-239-159-112.bu.edu>
2024-12-02 15:02:35 -0500

    more skeleton code, dockerfile *should* be good?

---
+++ trivia_game/app.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+from flask import Flask, jsonify, make_response, Response, request
+
+# Load environment variables from .env file
+load_dotenv()
+
+app = Flask(__name__)
+
+playlist_model = PlaylistModel()
+

8a892303d4d9d1f64a24c02de0ca4b225b5353a0
Anna LaPrade <annalaprade@crc-dot1x-nat-10-239-159-112.bu.edu>
2024-12-08 00:19:43 -0500

    updated database

--- trivia_game/sql/create_team_table.sql
+++ trivia_game/sql/create_team_table.sql
@@ -4,17 +4,14 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE teams (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     team TEXT NOT NULL UNIQUE,
-    current_score INTEGER DEFAULT 0,
-    favorite_game_category INTEGER,
-    deleted BOOLEAN DEFAULT FALSE,
-    password_hash TEXT NOT NULL,
     FOREIGN KEY (favorite_game_category) REFERENCES categories(id)
+    mascot TEXT,
+    deleted BOOLEAN DEFAULT FALSE,
+    current_score INTEGER DEFAULT 0,
+    games_played INTEGER DEFAULT 0,
+    total_score INTEGER DEFAULT 0,
+    
+    
 );
 
 
-CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    salt TEXT NOT NULL,
-    password_hash TEXT NOT NULL
-);
